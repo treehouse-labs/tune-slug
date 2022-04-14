146e04617d19e77ecf02c47156bbf6f202f421a3
hectorohernz <hectorohernz@gmail.com>
2022-04-14 19:10:06 -0400

    Exposing Env Vars

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    MY_VAR: process.env.MY_VAR
+    MY_VAR: process.env.MY_VAR,
+    NEXT_PUBLIC_SPOTIFY_CLIENT_ID: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+    NEXT_PUBLIC_SPOTIFY_AUTH_REDIRECT: process.env.NEXT_PUBLIC_SPOTIFY_AUTH_REDIRECT,
+    NEXT_PUBLIC_SPOTIFY_AUTH_ENDPOINT:process.env.NEXT_PUBLIC_SPOTIFY_AUTH_ENDPOINT
   }
 }
 
